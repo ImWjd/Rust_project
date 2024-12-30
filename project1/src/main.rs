085468bdd31a9c1b4e5fb64b9cadea19c4b53b91
ImWjd <1594803224@qq.com>
2024-12-30 13:13:45 +0800

    11

--- project1/src/main.rs
+++ project1/src/main.rs
@@ -1,17 +1,9 @@
 fn main() {
     println!("Hello, world!");
     let mut s = String::from("Hellow");
-    s.push_str(",worlld!!!");
+    s.push_str(",worlld!!!");// push_str 用于对字符进行拼接
     println!("{}",s);
-    println!("{}",s);
-    println!("{}",s);
-    println!("{}",s);
-    println!("{}",s);
-    println!("{}",s);
-    println!("{}",s);
-    println!("{}",s);
-    println!("{}",s);
-    println!("{}",s);
-    println!("{}",s);
+    // 变量和数据交互方式： 移动（move）
+   
     
 }
